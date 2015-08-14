8af61d220b62ff2a7bf6b35a246a8c0bfdf1820d
mhackemack <mhackemack@gmail.com>
2015-08-13 22:01:48 -0500

    started fourier energy work

---
+++ Codes/MATLAB/Fourier_Energy/Main_Searcher.m
@@ -0,0 +1,38 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Title:          Fourier Analysis Script - Search Method
+%
+%   Author:         Michael W. Hackemack
+%   Institution:    Texas A&M University
+%   Year:           2015
+%   
+%   Description:    
+%   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Note(s):        
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Clear Project Space
+% -------------------
+if exist('pbool', 'var')
+    clearvars -except pbool
+else
+    clear; pbool = false;
+end
+clc; close all; %format long e
+if ~pbool, fpath = get_path(); addpath(fpath); pbool = true; end
+% Define Path
+% -----------
+global glob
+glob = get_globals('Home');
+glob.print_info = false;
+% Define all user inputs
+% ------------------------------------------------------------------------------
+
+% end user input section
+% ------------------------------------------------------------------------------
+% Populate data and output structures
+% -----------------------------------
+print_FA_heading();
+
---
+++ Codes/MATLAB/Fourier_Energy/get_globals.m
@@ -0,0 +1,56 @@
+function out = get_globals( s )
+% Numbers
+%%%%%%%%%
+out.small = 1e-14;
+out.large = 1/eps/10;
+if strcmp(s, 'Home')
+    % Home - more memory
+    out.maxMatrix = 2.5e4;
+    out.maxSparse = 1.1e5;
+else
+    % Office
+    out.maxMatrix = 1e4;
+    out.maxSparse = 6e4;
+end
+% Constants
+%%%%%%%%%%%
+out.speed_of_light = 2.99792458e10;
+out.planck = 6.62606957e-34;
+out.boltzmann_ev = 8.6173324e-5;
+out.boltzmann_J = 1.3806488e-23;
+out.boltzmann_erg = 1.3806488e-16;
+out.elementary_charge = 1.6021892e-19;
+out.neutron_mass = 1.6749544e-27;
+out.avogadro = 6.022141e23;
+out.gas_constant = 8.3144621;
+
+out.amu_to_kg = 1.6605655e-27;
+out.amu_to_MeV = 931.5016;
+out.MeV_to_J = 1.601892e-13;
+out.J_to_MeV = 6.2415065e12;
+% Miscellaneous
+%%%%%%%%%%%%%%%
+out.input_path = 'user_inputs/';
+out.output_path = '../Outputs/';
+out.geom_path = '/geometry_inputs/precompiled/';
+out.geom_raw_tri_tet_path = 'geometry_inputs/raw_tri_tet_files/';
+out.geom_raw_dg_path = 'geometry_inputs/raw_dg_files/';
+out.directory = [];
+out.print_info = true;
+% Global Face Condition Types
+% ---------------------------
+out.Periodic = -1;
+out.Interior = 0;
+% Boundary Conditions
+% -------------------
+out.Dirichlet = 1;
+out.Neumann = 2;
+out.Robin = 3;
+out.Function = 4;
+% Transport Boundary Conditions
+% -----------------------------
+out.Vacuum = 1;
+out.Reflecting = 2;
+out.IncidentIsotropic = 3;
+out.IncidentCurrent = 4;
+out.IncidentBeam = 5;
