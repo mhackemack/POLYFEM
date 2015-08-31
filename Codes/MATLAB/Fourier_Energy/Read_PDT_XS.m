ee32e84245049566903866b01ed1fb3d2298da01
mhackemack <mhackemack@gmail.com>
2015-08-31 07:07:11 -0500

    small

---
+++ Codes/MATLAB/Fourier_Energy/Read_PDT_XS.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Title:          PDT XS Reader (Script)
+%
+%   Author:         Michael W. Hackemack
+%   Institution:    Texas A&M University
+%   Year:           2015
+%   
+%   Description:    MATLAB script to read PDT XS files and pull pertinent 
+%                   data out. The main XS of interest are the total XS and 
+%                   the scattering matrices.
+%   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Note(s):        
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear; clc; close all; format long e
+
