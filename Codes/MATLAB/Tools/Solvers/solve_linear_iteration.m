40dddd8cb24beb828a8f76ae49a0bdcf23fd316b
mhackemack <mhackemack@gmail.com>
2015-09-25 08:14:21 -0500

    added linear iteration function

---
+++ Codes/MATLAB/Tools/Solvers/solve_linear_iteration.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Title:          Solve Linear Iteration
+%
+%   Author:         Michael W. Hackemack
+%   Institution:    Texas A&M University
+%   Year:           2015
+%   
+%   Description:    
+%   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Note(s):        
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [data, sol] = solve_linear_iteration(data,mesh,DoF,FE,sol)
