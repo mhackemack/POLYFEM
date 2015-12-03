146cf60b02662315299005e97926747c37e0596e
mhackemack <mhackemack@gmail.com>
2015-12-03 12:52:28 -0600

    small

---
+++ Codes/MATLAB/Fourier_Energy/Main_InfMedium.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Title:          XS Fourier Analysis Script
+%
+%   Author:         Michael W. Hackemack
+%   Institution:    Texas A&M University
+%   Year:           2015
+%   
+%   Description:    
+%   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Note(s):        
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear; clc; close all; format long e
+% Clear Project Space
+% ------------------------------------------------------------------------------
+if ~pbool, fpath = get_path(); addpath(fpath); pbool = true; end
+global glob; glob = get_globals('Home');
+inp = '';
+addpath(['inputs/',inp]); % This one must be last to properly switch input files
+% ------------------------------------------------------------------------------
