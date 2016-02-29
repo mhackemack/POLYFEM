342bacd3e3226c68322634b1f22857f1ced37c82
mhackemack <mhackemack@gmail.com>
2016-02-29 17:26:35 -0600

    fourier PHI working
    will run overnight later to collect data

--- Codes/MATLAB/Fourier/Main_PHI.m
+++ Codes/MATLAB/Fourier/Main_PHI.m
@@ -29,18 +29,85 @@ glob = get_globals('Home');
 glob.print_info = false;
 % Load all user inputs
 % ------------------------------------------------------------------------------
-inp = '1D_99G_WGS_DSA'; addpath([glob.input_path,inp]);
-data = load_user_input();
-% additional inputs
-data.Type = 'Search';
-data.NumberPhasePerDim = 3;
-
+inp = '2D_PHI'; addpath([glob.input_path,inp]);
+% Problem inputs
+sigt = [10,20,40,80,160,320,640];
+c    = [0.9,0.99,0.999,0.9999,0.99999,0.999999];
+nstrips = 2;
+data = load_user_input(nstrips);
 % end user input section
 % ------------------------------------------------------------------------------
-% Populate data and output structures
-% -----------------------------------
+% Compute all solutions
+% ---------------------
 print_FA_heading(data);
-
-% Retrieve all spectrum data and postprocess
+b_func = get_build_function(data);
+outdir = sprintf('outputs/PHI/%dD/%s/',data.problem.Dimension,data.geometry.type);
+if data.Neutronics.FEMLumping
+    lump = 'L';
+else
+    lump = 'U';
+end
+if ~data.Neutronics.PerformAcceleration % Unaccelerated
+    outname = sprintf('%s_%s',data.Neutronics.TransportMethod,data.Neutronics.SpatialMethod);
+elseif data.Neutronics.PerformAcceleration % Accelerated
+    outname = sprintf('%s_%s_C=%d_%s%s%d',data.Neutronics.TransportMethod,data.Neutronics.DSAType,data.Neutronics.IP_Constant,lump,data.Neutronics.SpatialMethod,data.Neutronics.FEMDegree);
+end
+if ~isequal(exist(outdir, 'dir'),7),mkdir(outdir); end
+SR = zeros(length(sigt),length(c),length(data.Neutronics.Transport.SnLevels));
+% Loop through total cross sections
+for t=1:length(sigt)
+    data = load_user_input(nstrips);
+    for cc=1:length(c)
+        data = set_phi_xs(data, sigt(t), c(cc));
+        % Build input space
+        [data, inputs] = process_fourier_inputs( data );
+        % Run the Search problems
+        data.Type = 'Search';
+        data.NumberPhasePerDim = 5;
+        inputs = build_phase_transformation_matrix(data, inputs);
+        outputs = calculate_eigenspectrums(data, inputs);
+        % Process Search outputs - also calculate maximum case for eigenspectrum
+        for q=1:length(data.Neutronics.Transport.SnLevels)
+            qlvl = data.Neutronics.Transport.SnLevels(q);
+            % Save maximum eigenvalue - compute largest eigenvalue distribution
+            SR(t,cc,q) = outputs{q,1}.Eigen.Max;
+            [inp, ~] = combine_input_set(data, inputs, 1, q);
+            P = b_func(outputs{q,1}.Eigen.MaxLambda,inp);
+            [V,D] = eig(P); D=diag(D);
+            % Save output data
+            fulloutname = sprintf('%s_%s%d_sigt=%d_c=%g',outname,data.Neutronics.Transport.QuadType,qlvl,sigt(t),c(cc));
+            dlmwrite([outdir,fulloutname,'_Eigenvalues.dat'],[D,real(D),imag(D)]);
+            dlmwrite([outdir,fulloutname,'_Eigenvectors.dat'],V);
+        end
+        % Run the Grid problems
+        data.Type = 'Grid';
+        data.NumberPhasePerDim = 101;
+        inputs = build_phase_transformation_matrix(data, inputs);
+        outputs = calculate_eigenspectrums(data, inputs);
+        % Process Grid outputs
+        for q=1:length(data.Neutronics.Transport.SnLevels)
+            qlvl = data.Neutronics.Transport.SnLevels(q);
+            % Make contour plot
+            x = inputs.phase{q,1}.WN{1}; x = x./max(max(x))*2*pi;
+            y = inputs.phase{q,1}.WN{2}; y = y./max(max(y))*2*pi;
+            z = outputs{q,1}.Eigen.Grid;
+            contourf(x,y,z,20); colorbar;
+            xlabel('\lambda_x');
+            ylabel('\lambda_y');
+            set(gca,'FontSize',11);
+%             tightfig(gcf); colorbar;
+            % Save contour plot
+            fulloutname = sprintf('%s_%s%d_sigt=%d_c=%g',outname,data.Neutronics.Transport.QuadType,qlvl,sigt(t),c(cc));
+            savefig(gcf,[outdir,fulloutname,'_contour.fig']);
+            print(gcf,'-dpng',[outdir,fulloutname,'_contour.png']);
+            print(gcf,'-depsc',[outdir,fulloutname,'_contour.eps']);
+            close(gcf);
+        end
+    end
+end
+% Save off maximum spectral radii information
 % ------------------------------------------------------------------------------
-
+for q=1:length(data.Neutronics.Transport.SnLevels)
+    fulloutname = sprintf('%s_%s%d',outname,data.Neutronics.Transport.QuadType,qlvl);
+    dlmwrite([outdir,fulloutname,'_SpectralRadii.dat'],[0,c;sigt',SR]);
+end
---
+++ Codes/MATLAB/Fourier/user_inputs/2D_PHI/set_phi_xs.m
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Title:          Set PHI Cross Sections
+%
+%   Author:         Michael W. Hackemack
+%   Institution:    Texas A&M University
+%   Year:           2015
+%   
+%   Description:    
+%   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Note(s):        
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function data = set_phi_xs(data, sigt, c)
+% Set Cross Sections
+sigt = [sigt;1/sigt];
+data.Neutronics.TotalXS = sigt;
+data.Neutronics.DiffusionXS = (1/3)./sigt;
+data.Neutronics.ScatteringXS = c*sigt;
+data.Neutronics.AbsorbXS = (1-c)*sigt;
+% Set Average Cross Sections
+data.Neutronics.AveTotalXS      = data.Neutronics.TotalXS;
+data.Neutronics.AveDiffusionXS  = data.Neutronics.DiffusionXS;
+data.Neutronics.AveScatteringXS = data.Neutronics.ScatteringXS;
+data.Neutronics.AveAbsorbXS     = data.Neutronics.AbsorbXS;
