77240392b0f4f0c73595db54d33401f3d5c67e5c
mhackemack <mhackemack@gmail.com>
2016-03-11 08:18:45 -0600

    more PA

--- Codes/MATLAB/POLYFEM/Main_Transport_PureAbsorber.m
+++ Codes/MATLAB/POLYFEM/Main_Transport_PureAbsorber.m
@@ -22,7 +22,7 @@ inp = 'Transport_PureAbsorber';
 addpath([glob.input_path,inp]); % This one must be last to properly switch input files
 % Being User Input Section
 % ------------------------------------------------------------------------------
-prob_name = 'IncidentLeftFace_2D_45degDown_LS4';
+prob_name = 'IncidentLeftTopFace_2D_45degDown_LS4';
 tri_run_bool  = true;
 cart_run_bool = true;
 poly_run_bool = true;
@@ -37,19 +37,19 @@ geom_in.Lz = 1; geom_in.ncellz = 4;
 geom_in.xmin_bound_type = glob.Function;
 geom_in.xmax_bound_type = glob.Vacuum;
 geom_in.ymin_bound_type = glob.Vacuum;
-geom_in.ymax_bound_type = glob.Vacuum;
+geom_in.ymax_bound_type = glob.Function;
 geom_in.zmin_bound_type = glob.Reflecting;
 geom_in.zmax_bound_type = glob.Reflecting;
 geom_in.xmin_val = @BoundaryFunc_IncidentLeftFace_2D_45degDown_LS4;
 geom_in.xmax_val = 0;
 geom_in.ymin_val = 0;
-geom_in.ymax_val = 0;
+geom_in.ymax_val = @BoundaryFunc_IncidentLeftFace_2D_45degDown_LS4;
 geom_in.zmin_val = 0;
 geom_in.zmax_val = 0;
 % ---
 % sdm = {'PWLD'};
 sdm = {'PWLD','WACHSPRESS','MV','MAXENT'};
-fedeg = [2];
+fedeg = [1,2];
 dat_in.SpatialMethod = 'PWLD';
 dat_in.FEMDegree = 2;
 dat_in.FEMLumping = false;
