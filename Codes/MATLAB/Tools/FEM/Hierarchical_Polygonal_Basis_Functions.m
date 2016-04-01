3d66fe5ff578d4c485439f21c0ce1ebd157cdc56
mhackemack <mhackemack@gmail.com>
2016-04-01 16:43:29 -0500

    small

---
+++ Codes/MATLAB/Tools/FEM/Hierarchical_Polygonal_Basis_Functions.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Title:          Hierarchical Basis Function Generator
+%
+%   Author:         Michael W. Hackemack
+%   Institution:    Texas A&M University
+%   Year:           2014
+%
+%   Description:    
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Notes:          
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function varargout = Hierarchical_Polygonal_Basis_Functions(varargin)
