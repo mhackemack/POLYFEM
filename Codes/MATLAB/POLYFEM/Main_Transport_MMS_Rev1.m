c167ac517215552e8b1b50644367633742fa87de
mhackemack <mhackemack@gmail.com>
2016-04-13 18:56:21 -0500

    small

--- Codes/MATLAB/POLYFEM/Main_Transport_MMS_Rev1.m
+++ Codes/MATLAB/POLYFEM/Main_Transport_MMS_Rev1.m
@@ -36,13 +36,13 @@ geom_in.Lx = 1; geom_in.ncellx = 4;
 geom_in.Ly = 1; geom_in.ncelly = 4;
 % ---
 fedeg = [2];
-sdm = {'MAXENT'};
+sdm = {'PWLD','MV'};
 % fedeg = [1,2];
 % sdm = {'PWLD','MV','MAXENT'};
 % ---
-dat_in.lvls = 6;
+dat_in.lvls = 18;
 dat_in.irr = 1;
-dat_in.tol = 0.0;
+dat_in.tol = 0.1;
 % Execute Problem Suite
 % ------------------------------------------------------------------------------
 print_heading(now, date);
@@ -60,6 +60,18 @@ for k=1:length(fedeg)
         [data, sol, ~, ~, ~] = execute_problem(data, geometry);
         [dofs,err] = retrieve_MMS_error(data,sol);
         dlmwrite(['outputs/',data.problem.Path,'/',data.problem.Name,'_mmserror','.dat'],[dofs,err],'precision','%18.14e');
+        maxv = 0;
+        for i=0:data.problem.refinementLevels
+            if length(sol{i+1}.CellVertexNumbers) > maxv
+                maxv = length(sol{i+1}.CellVertexNumbers);
+            end
+        end
+        cellverts = zeros(data.problem.refinementLevels+1, maxv);
+        for i=0:data.problem.refinementLevels
+            nv = length(sol{i+1}.CellVertexNumbers);
+            cellverts(i+1,1:nv) = sol{i+1}.CellVertexNumbers;
+        end
+        dlmwrite(['outputs/',data.problem.Path,'/',data.problem.Name,'_numcellverts','.dat'],cellverts);
     end
 end
 
